47b64cf9dd2e69ebe702ecf71333aeb2d24d91a8
Rick Solo <mrfox7398@mail.ru>
2022-11-18 18:42:55 +0400

    Improvements

--- User_interface.py
+++ User_interface.py
@@ -1,4 +1,5 @@
 def get_info ():
+
     info = []
     last_name = input('Введите фамилию: ')
     info.append(last_name)
@@ -8,13 +9,17 @@ def get_info ():
     valid =False
     while not valid:
         try:
+
             phone_number = input('Введите номер телефона: ')
             if len(phone_number) != 11:
+
                 print('В номере телефона должно быть 11 цифр.')
             else:
+
                 phone_number = int(phone_number)
                 valid = True
         except:
+            
             print('Номер телефона должен состоять только из цифр.')
     info.append(phone_number)
     description = input('Введите описание: ')
